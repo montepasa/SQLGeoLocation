1256b646e9a4a2e3f84924588028aa78547c095a
montepasa <ibrahimsamedaker@gmail.com>
2016-11-23 16:46:39 +0300

    Update sql geography.sql

--- sql geography.sql
+++ sql geography.sql
@@ -1,7 +1,9 @@
 CREATE TABLE [dbo].[ibrahim]
 (
     [Id] BIGINT NOT NULL PRIMARY KEY IDENTITY, 
-    //	/*buraya değişkenlerinizi ekliyorsunuz. Latitude ve Longitude alanlarına float ile ve virgül ile ayırılmış şekilde girilen değerler (örnek veriyorum 36,75435 ve 30,34566) artık Location alanına ve geography formatında otomatik olarak yazılır. İlerde bu Location alanını kullanarak uzaklık hesaplamaları yapabilirsiniz */
+    //	/*buraya değişkenlerinizi ekliyorsunuz. Latitude ve Longitude alanlarına float değer ve virgül ile ayırılmış şekilde 
+    girilen konumlar (örnek veriyorum 36,75435 ve 30,34566) artık Location alanına ve geography formatında otomatik olarak yazılır. 
+    İlerde bu Location alanını kullanarak uzaklık hesaplamaları yapabilirsiniz */
     [Latitude] float NOT NULL, 
     [Longitude] float NOT NULL, 
     [Location]  AS ([geography]::Point(ISNULL(Longitude, ''),ISNULL(Latitude, ''), 4326))
